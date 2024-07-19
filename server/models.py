b9fd834a124f6f788f6be2a0aa932ffc11c0c09d
Derrick <riobad74@gmail.com>
2024-07-19 21:59:54 +0300

    tests pass

--- server/models.py
+++ server/models.py
@@ -10,7 +10,7 @@ class Message(db.Model, SerializerMixin):
     body = db.Column(db.String)
     username = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
+    updated_at = db.Column(db.DateTime, server_default=db.func.now())
+    
     def __repr__(self):
         return f'<Message by {self.id} - {self.username}>'
